a839a95ea1b1507796c4ed7dff03a0971ddff4c0
Brian Yngve <67799535+OminousPotato@users.noreply.github.com>
2022-10-22 13:56:09 -0400

    [Skeleton] Import project skeleton

---
+++ SustainabotMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SustainabotMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
